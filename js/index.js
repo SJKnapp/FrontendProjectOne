e4a20e8cbb09acba96a997db5b3e1bcef21714f3
SJKnapp <samuel.j.knapp@outlook.com>
2021-07-28 11:05:04 +0100

    PROJ-32 #comment added display of all varibles and added save and delete buttons that do not currently function

--- js/index.js
+++ js/index.js
@@ -2,16 +2,41 @@
 
 (function() {
 
-    const baseUrl="http://127.0.0.1:8080";
+    const baseUrl='http://127.0.0.1:8080';
 
     const displayAllTasks = document.querySelector("#renderedTasks");
 
     const renderTask = (task) => {
         const renderedTask = document.createElement('div');
-        const taskName = document.createElement('p');
-        taskName.textContent = task.name;
 
+        const taskName = document.createElement('p');
+        taskName.classList =  'inline';
+        taskName.textContent = task.name;
         renderedTask.appendChild(taskName);
+
+        const taskDesciption = document.createElement('p');
+        taskDesciption.textContent = task.description;
+        renderedTask.appendChild(taskDesciption);
+
+        const taskpriority = document.createElement('p');
+        taskpriority.textContent = task.dueDate;
+        renderedTask.appendChild(taskpriority);
+
+        const taskTimeEstimate = document.createElement('p');
+        taskTimeEstimate.textContent = task.timeEstimateMinutes;
+        renderedTask.appendChild(taskTimeEstimate);
+
+        const taskDueDate = document.createElement('p');
+        taskDueDate.textContent = task.dueDate;
+        renderedTask.appendChild(taskDueDate);    
+
+        const saveButton = document.createElement('button');
+        saveButton.textContent = 'save';
+        renderedTask.appendChild(saveButton); 
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'delete';
+        renderedTask.appendChild(deleteButton); 
 
         displayAllTasks.appendChild(renderedTask);
     }
