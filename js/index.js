cb5d8fdadb980269e9e354d044d44894eeab6b46
SJKnapp <samuel.j.knapp@outlook.com>
2021-07-29 11:12:00 +0100

    PROJ-87 #comment Added a favicon aswell as used bootstrap to place all of the items in rows and columns adding titles to the page in order to keep them allinged to the title

--- js/index.js
+++ js/index.js
@@ -38,30 +38,38 @@
     }
 
     const renderTask = (task, empty) => {
+
         const renderedTask = document.createElement('div');
+        renderedTask.classList = "row align-self-start";
 
         const taskName = document.createElement('input');
+        taskName.classList = "col-1";
         taskName.value = task.name;
         renderedTask.appendChild(taskName);
 
         const taskDesciption = document.createElement('input');
+        taskDesciption.classList = "col";
         taskDesciption.value = task.description;
         renderedTask.appendChild(taskDesciption);
 
         const taskpriority = document.createElement('input');
+        taskpriority.classList = "col";
         taskpriority.value = task.priority;
         renderedTask.appendChild(taskpriority);
 
         const taskTimeEstimate = document.createElement('input');
+        taskTimeEstimate.classList = "col";
         taskTimeEstimate.value = task.timeEstimateMinutes;
         renderedTask.appendChild(taskTimeEstimate);
 
         const taskDueDate = document.createElement('input');
+        taskDueDate.classList = "col";
         taskDueDate.type="date";
         taskDueDate.value = task.dueDate;
         renderedTask.appendChild(taskDueDate);    
 
         const saveButton = document.createElement('button');
+        saveButton.classList = "col";
         saveButton.textContent = 'save';
         renderedTask.appendChild(saveButton); 
         saveButton.addEventListener('click', () =>{
@@ -82,12 +90,17 @@
 
         if(!empty){
             const deleteButton = document.createElement('button');
+            deleteButton.classList = 'col';
             deleteButton.textContent = 'delete';
             deleteButton.addEventListener('click', () => {
                 deleteTask(task.id);
             })
             renderedTask.appendChild(deleteButton); 
-        }
+        }else{
+			const emptyEllement = document.createElement('div');
+			emptyEllement.classList = 'col';
+			renderedTask.appendChild(emptyEllement)
+		}
         displayAllTasks.appendChild(renderedTask);
     }
 
